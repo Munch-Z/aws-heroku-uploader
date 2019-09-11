3abadae2351f4d5f82efb8bc3ff968fcab9772ea
Munch-Z <46868472+Munch-Z@users.noreply.github.com>
2019-09-11 16:32:40 -0700

    AWS Calls Completed (just need to write one for deleting, but should be easy.)

--- server.js
+++ server.js
@@ -83,50 +83,161 @@ app.get("/account", (req, res) => {
   });
 });
 
-app.get("/signin", (req, res) => {
-  //   get password from DB
-  db.User.findOne({
-    where: {
-      username: "Zamurph"
+app.get("/collection", (req, res) => {
+  res.sendFile(path.join(__dirname, "./views/accounts.html"));
+
+  //gets info from database NEEDS TO BE PARSED INTO JSON
+  s3.getObject({ Bucket: S3_BUCKET, Key: USERID + "/collection.json" }, function(
+    err,
+    data
+  ) {
+    if (err) {
+      console.log("Error", err);
+    } else {
+      console.log(
+        "This is the content of the file that you are requesting: ",
+        JSON.parse(data.Body)
+      );
     }
-  })
-    .then(data => {
-      //compare incoming password to hashed password from DB
-      bCrypt.compare("testing", data.password, (err, match) => {
-        if (err) throw err;
-
-        if (match)
-          res.sendFile(path.join(__dirname, "./views/authorized.html"));
-      });
-    })
-    .catch(err => {
-      console.log(err);
-    });
-
-  res.sendFile(path.join(__dirname, "./views/signin.html"));
-});
-
-app.post("/createUser", (req, res) => {
-  const password = req.body.password;
-  //hash password
-  bCrypt.hash(password, 10, (err, hash) => {
-    if (err) throw err;
-
-    const newUser = {
-      username: req.body.username,
-      password: hash,
-      secret_answer: req.body.secret
-    };
-    //store hashed pass in the DB
-    db.User.create(newUser)
-      .then(data => {
-        console.log(data);
-      })
-      .catch(err => {
-        console.log(err);
-      });
   });
 });
+
+app.get("/decks", (req, res) => {
+  res.sendFile(path.join(__dirname, "./views/accounts.html"));
+
+  //gets info from database NEEDS TO BE PARSED INTO JSON
+  // s3.getObject({ Bucket: S3_BUCKET, Key: USERID + "/decks/" }, function(
+  //   err,
+  //   data
+  // ) {
+  //   if (err) {
+  //     console.log("Error", err);
+  //   } else {
+  //     console.log(
+  //       "This is the content of the file that you are requesting: ",
+  //       JSON.parse(data.Body)
+  //     );
+  //   }
+  // });
+
+  s3.listObjects({Bucket: S3_BUCKET, Prefix: USERID + '/decks/', Delimiter: '/'}, (err, decks) => {
+    if (err) throw err;
+
+    console.log(decks.CommonPrefixes);
+
+    console.log(getDeckNames(decks.CommonPrefixes));
+  })
+});
+
+
+function getDeckNames(arr) {
+  const decks = [];
+  for(let i=0; i<arr.length; i++) {
+    decks.push(arr[i].Prefix.split('/')[2]);
+  }
+
+  return decks;
+}
+
+app.get("/decklist", (req, res) => {
+  res.sendFile(path.join(__dirname, "./views/accounts.html"));
+
+  //gets info from database NEEDS TO BE PARSED INTO JSON
+  s3.getObject({ Bucket: S3_BUCKET, Key: USERID + "/decks/" + req.body.deckname + "/decklist.json" }, function(
+    err,
+    data
+  ) {
+    if (err) {
+      console.log("Error", err);
+    } else {
+      console.log(
+        "This is the content of the file that you are requesting: ",
+        JSON.parse(data.Body)
+      );
+    }
+  });
+});
+
+// app.get("/signin", (req, res) => {
+//   //   get password from DB
+//   db.User.findOne({
+//     where: {
+//       username: "Zamurph"
+//     }
+//   })
+//     .then(data => {
+//       //compare incoming password to hashed password from DB
+//       bCrypt.compare("testing", data.password, (err, match) => {
+//         if (err) throw err;
+
+//         if (match)
+//           res.sendFile(path.join(__dirname, "./views/authorized.html"));
+//       });
+//     })
+//     .catch(err => {
+//       console.log(err);
+//     });
+
+//   res.sendFile(path.join(__dirname, "./views/signin.html"));
+// });
+
+// app.post("/createUser", (req, res) => {
+//   const password = req.body.password;
+//   //hash password
+//   bCrypt.hash(password, 10, (err, hash) => {
+//     if (err) throw err;
+
+//     const newUser = {
+//       username: req.body.username,
+//       password: hash,
+//       secret_answer: req.body.secret
+//     };
+//     //store hashed pass in the DB
+//     db.User.create(newUser)
+//       .then(data => {
+//         console.log(data);
+//       })
+//       .catch(err => {
+//         console.log(err);
+//       });
+//   });
+// });
+
+app.post("/createcollection", (req, res) => {
+  //PARAMS OBJECKT FOR UPLOAD REQUEST
+  const params = {
+    Bucket: S3_BUCKET,
+    Key: USERID + "/collection.json",
+    Body: JSON.stringify(req.body.collection)
+  };
+
+  //UPLOAD FILE TO AWS AND THEN SEND IT BACK
+  s3.upload(params, (s3Err, data) => {
+    if (s3Err) throw s3Err;
+    console.log(`File uploaded successfully at ${data.Location}`);
+    res.sendStatus(200);
+  });
+});
+
+app.post("/createdeck", (req, res) => {
+  //PARAMS OBJECKT FOR UPLOAD REQUEST
+
+  console.log(req.body);
+  
+  const params = {
+    Bucket: S3_BUCKET,
+    Key: USERID + "/decks/" + req.body.deckname + "/decklist.json",
+    Body: JSON.stringify(req.body.decklist)
+  };
+
+  //UPLOAD FILE TO AWS AND THEN SEND IT BACK
+  s3.upload(params, (s3Err, data) => {
+    if (s3Err) throw s3Err;
+    console.log(`File uploaded successfully at ${data.Location}`);
+    res.sendStatus(200);
+  });
+});
+
 
 app.post("/create", (req, res) => {
   //PARAMS OBJECKT FOR UPLOAD REQUEST
@@ -140,6 +251,6 @@ app.post("/create", (req, res) => {
   s3.upload(params, (s3Err, data) => {
     if (s3Err) throw s3Err;
     console.log(`File uploaded successfully at ${data.Location}`);
-    res.json(req.body);
+    res.sendStatus(200);
   });
 });
